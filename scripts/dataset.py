0024c6d7f8634de09e0e7950f9cf8946269816f5
shuanchen <shuankaist@kaist.ac.kr>
2021-06-23 15:48:12 +0900

    add configs

---
+++ scripts/dataset.py
@@ -0,0 +1,70 @@
+import os
+import pandas as pd
+
+from rdkit import Chem
+
+import torch
+import sklearn
+import dgl.backend as F
+from dgl.data.utils import save_graphs, load_graphs
+
+class USPTODataset(object):
+    def __init__(self, args, smiles_to_graph, node_featurizer, edge_featurizer, load=True, log_every=1000):
+        df = pd.read_csv('%s/labeled_data.csv' % args['data_dir'])
+        self.train_ids = df.index[df['Split'] == 'train'].values
+        self.val_ids = df.index[df['Split'] == 'val'].values
+        self.test_ids = df.index[df['Split'] == 'test'].values
+        self.smiles = df['Products'].tolist()
+        self.atom_labels = [eval(t) for t in df['Atom_label']]
+        self.bond_labels = [eval(t) for t in df['Bond_label']]
+        self.cache_file_path = '../data/saved_graphs/%s_dglgraph.bin' % args['dataset']
+        self._pre_process(smiles_to_graph, node_featurizer, edge_featurizer, load, log_every)
+
+    def _pre_process(self, smiles_to_graph, node_featurizer, edge_featurizer, load, log_every):
+        if os.path.exists(self.cache_file_path) and load:
+            print('Loading previously saved dgl graphs...')
+            self.graphs, label_dict = load_graphs(self.cache_file_path)
+        else:
+            print('Processing dgl graphs from scratch...')
+            self.graphs = []
+            for i, s in enumerate(self.smiles):
+                if (i + 1) % log_every == 0:
+                    print('\rProcessing molecule %d/%d' % (i+1, len(self.smiles)), end='', flush=True)
+                self.graphs.append(smiles_to_graph(s, node_featurizer=node_featurizer,
+                                                   edge_featurizer=edge_featurizer, canonical_atom_order=False))
+            print ()
+            save_graphs(self.cache_file_path, self.graphs)
+
+    def __getitem__(self, item):
+        return self.smiles[item], self.graphs[item], self.atom_labels[item], self.bond_labels[item]
+
+    def __len__(self):
+            return len(self.smiles)
+        
+class USPTOTestDataset(object):
+    def __init__(self, args, smiles_to_graph, node_featurizer, edge_featurizer, load=True, log_every=1000):
+        df = pd.read_csv('%s/raw_test.csv' % args['data_dir'])
+        self.rxns = df['reactants>reagents>production'].tolist()
+        self.smiles = [rxn.split('>>')[-1] for rxn in self.rxns]
+        self.cache_file_path = '../data/saved_graphs/%s_test_dglgraph.bin' % args['dataset']
+        self._pre_process(smiles_to_graph, node_featurizer, edge_featurizer, load, log_every)
+
+    def _pre_process(self, smiles_to_graph, node_featurizer, edge_featurizer, load, log_every):
+        if os.path.exists(self.cache_file_path) and load:
+            print('Loading previously saved test dgl graphs...')
+            self.graphs, label_dict = load_graphs(self.cache_file_path)
+        else:
+            print('Processing test dgl graphs from scratch...')
+            self.graphs = []
+            for i, s in enumerate(self.smiles):
+                if (i + 1) % log_every == 0:
+                    print('Processing molecule %d/%d' % (i+1, len(self.smiles)))
+                self.graphs.append(smiles_to_graph(s, node_featurizer=node_featurizer,
+                                                   edge_featurizer=edge_featurizer, canonical_atom_order=False))
+            save_graphs(self.cache_file_path, self.graphs)
+
+    def __getitem__(self, item):
+            return self.smiles[item], self.graphs[item], self.rxns[item]
+
+    def __len__(self):
+            return len(self.smiles)
